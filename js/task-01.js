32bc539ce2e912e3edf8f1d9717de666d498ab24
SashaShulga94 <alexshulga.ss@gmail.com>
2022-11-30 13:53:00 +0200

    task1 complete

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,7 @@
-const categoriesAll = document.querySelector("ul#categories");
-console.log(categoriesAll);
+const categories = document.querySelectorAll("li.item");
+console.log(`Number of categories: ${categories.length}`);
+
+categories.forEach((category) => {
+  console.log(`Category: ${category.firstElementChild.textContent}`);
+  console.log(`Elements: ${category.lastElementChild.children.length}`);
+});
